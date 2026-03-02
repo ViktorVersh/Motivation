9f14c8942e45477ae0334f8e7199a97dc302a695
ViktorVersh <vershininzaig@mail.ru>
2026-03-02 20:16:48 +0800

    смайл

--- keyboard.py
+++ keyboard.py
@@ -4,7 +4,7 @@ stinfo = ReplyKeyboardMarkup(resize_keyboard=True,
                              input_field_placeholder="🟢Воспользуйтесь меню:",
                              keyboard=[
                                  [KeyboardButton(text='✅Мотивация для Вас'),
-                                  KeyboardButton(text='📝Дополнить базу')],
+                                  KeyboardButton(text='🖊Дополнить базу')],
                                  [KeyboardButton(text='📖Информация о боте'),]
                              ]
                              )
